e1eff6245d7b0803d4591f2c4e1e7a1eef8d744d
rroyo <rroyog@uoc.edu>
2016-03-07 18:49:04 +0100

    primera prova de pujada d'arxius

---
+++ redditWhoBot.py
@@ -0,0 +1,17 @@
+# Guia per poder autentificar l'aplicació amb l'API de reddit
+# https://github.com/reddit/reddit/wiki/OAuth2
+
+
+#################################################################################
+#
+#	redditWho bot, v2016.03.07
+#
+#	author: Ramon Royo
+#	for:	Universitat Oberta de Catalunya TFG
+#
+#
+#
+#
+#
+#################################################################################
+
